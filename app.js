59d08fe8c9378a815041184e02194d0ca5ae046e
cass-2345 <es311608@gmail.com>
2025-06-06 11:19:05 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
+function criaCartao(categoria, pergunta, resposta) {
+        let container
+}
+function criaCartao(categoria, pergunta, resposta) {
+        let container = document.getElementById('container')
+}
+function criaCartao(categoria, pergunta, resposta) {
+        let container = document.getElementById('container')
+        let cartao = document.createElement('article')
+}
+function criaCartao(categoria, pergunta, resposta) {
+        let container = document.getElementById('container')
+        let cartao = document.createElement('article')
+        cartao.className = 'cartao'
+}
 
